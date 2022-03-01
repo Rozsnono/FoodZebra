69a1ca01dbbdc2a33863d9d0bdbc65c3327b75f1
rozsnono <rozsnono@gmail.com>
2022-03-01 10:40:00 +0100

    **BackEnd rewrite** Fixes #4

--- src/receipt/receipt.model.ts
+++ src/receipt/receipt.model.ts
@@ -14,6 +14,7 @@ const receiptSchema = new Schema<Receipt>(
         ingredients: Object,
         author: String,
         rate: Number,
+        pic: String,
     },
     { versionKey: false },
 );
