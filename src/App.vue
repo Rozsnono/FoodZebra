988f4c47977d8066f1812948de2750d8f1185ab7
rozsnono <rozsnono@gmail.com>
2022-03-07 11:11:28 +0100

    **New Menu** Fixes #29

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,137 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+  import { computed, ref } from "vue";
 
-import {
-  VApp,
-  VAppBar,
-  VAppBarNavIcon,
-  VBadge,
-  VBtn,
-  VContainer,
-  VList,
-  VListItem,
-  VMain,
-  VNavigationDrawer,
-  VSpacer,
-} from "vuetify/components";
-import { useDisplay } from "vuetify";
-import { useI18n } from "vue-i18n";
-import { useUsersStore } from "./store/usersStore";
+  import {
+    VApp,
+    VAppBar,
+    VAppBarNavIcon,
+    VBadge,
+    VBtn,
+    VContainer,
+    VList,
+    VListItem,
+    VMain,
+    VNavigationDrawer,
+    VSpacer,
+  } from "vuetify/components";
+  import { useDisplay } from "vuetify";
+  import { useI18n } from "vue-i18n";
+  import { useUsersStore } from "./store/usersStore";
 
-const isMobileDevice = useDisplay().mobile.value;
-sessionStorage.removeItem('currentUser');
-const drawer = ref(!isMobileDevice);
-const usersStore = useUsersStore();
-const theme = ref(false);
-const themeForApp = ref("light");
+  const isMobileDevice = useDisplay().mobile.value;
+  sessionStorage.removeItem("currentUser");
+  const drawer = ref(!isMobileDevice);
+  const usersStore = useUsersStore();
+  const theme = ref(false);
+  const themeForApp = ref("light");
 
-const loggedUser = computed(() => usersStore.getLoggedUser);
-console.log(loggedUser);
-const notLoggedIn = computed(() => usersStore.getLoggedUser == null);
+  const loggedUser = computed(() => usersStore.getLoggedUser);
+  console.log(loggedUser);
+  const notLoggedIn = computed(() => usersStore.getLoggedUser == null);
 
-let { locale, t } = useI18n({
-  inheritLocale: true,
-  useScope: "global", // Change to "local" if you want to add <i18n></i18n> locally
-});
-
-// Search icons: https://materialdesignicons.com/
-const menuItems = ref([
-  {
-    icon: "mdi-home",
-    text: t("startPage"),
-    name: "startPage",
-    route: "/",
-    disabled: false,
-  },
-  {
-    icon: "mdi-food",
-    text: t("food"),
-    name: "food",
-    route: "/food",
-    disabled: false,
-  },
-  {
-    icon: "mdi-food-fork-drink",
-    text: t("addFood"),
-    name: "addFood",
-    route: "/addFood",
-    disabled: notLoggedIn,
-  },
-  {
-    icon: "mdi-food-fork-drink",
-    text: t("modifyFood"),
-    name: "modifyFood",
-    route: "/modifyFood",
-    props: { asd: "asd" },
-    disabled: notLoggedIn,
-  },
-  {
-    icon: "mdi-information",
-    text: t("about"),
-    name: "about",
-    route: "/about",
-    disabled: false,
-  },
-  {
-    icon: "mdi-account",
-    text: t("account"),
-    name: "account",
-    route: "/account",
-    disabled: false,
-  },
-]);
-
-function toggleTheme() {
-  theme.value = theme.value === true ? false : true;
-  themeForApp.value = themeForApp.value === "light" ? "dark" : "light";
-}
-function toggleLanguage() {
-  locale.value = locale.value == "hu" ? "en" : "hu";
-  menuItems.value.forEach((e) => {
-    if (e.name != "") e.text = t(e.name);
+  let { locale, t } = useI18n({
+    inheritLocale: true,
+    useScope: "global", // Change to "local" if you want to add <i18n></i18n> locally
   });
-}
+
+  // Search icons: https://materialdesignicons.com/
+  const menuItems = ref([
+    {
+      icon: "mdi-home",
+      text: t("startPage"),
+      name: "startPage",
+      route: "/",
+      disabled: false,
+    },
+    {
+      icon: "mdi-food",
+      text: t("food"),
+      name: "food",
+      route: "/food",
+      disabled: false,
+    },
+    {
+      icon: "mdi-food-fork-drink",
+      text: t("addFood"),
+      name: "addFood",
+      route: "/addFood",
+      disabled: notLoggedIn,
+    },
+    {
+      icon: "mdi-food-fork-drink",
+      text: t("modifyFood"),
+      name: "modifyFood",
+      route: "/modifyFood",
+      props: { asd: "asd" },
+      disabled: notLoggedIn,
+    },
+    {
+      icon: "mdi-information",
+      text: t("about"),
+      name: "about",
+      route: "/about",
+      disabled: false,
+    },
+    {
+      icon: "mdi-account",
+      text: loggedUser.value ? loggedUser.value : t("account"),
+      name: "account",
+      route: "/account",
+      disabled: true,
+    },
+  ]);
+
+  function toggleTheme() {
+    theme.value = theme.value === true ? false : true;
+    themeForApp.value = themeForApp.value === "light" ? "dark" : "light";
+  }
+  function toggleLanguage() {
+    locale.value = locale.value == "hu" ? "en" : "hu";
+    menuItems.value.forEach((e) => {
+      if (e.name != "") e.text = t(e.name);
+    });
+  }
 </script>
 
 <template>
   <v-app :theme="themeForApp">
-    <v-navigation-drawer v-model="drawer" app :color="'primary'" dark>
-      <v-list dense nav :color="'primary'" dark>
-        <v-list-item
-          v-for="(item, i) in menuItems"
-          :key="i"
-          v-show="!item.disabled"
-          link
-          :prepend-icon="item.icon"
-          :title="item.text"
-          :to="item.route"
-          @click="drawer = !isMobileDevice"
-        ></v-list-item>
+    <v-navigation-drawer permanent v-model="drawer" app :color="'primary'" dark>
+      <v-system-bar></v-system-bar>
+      <v-list :color="'primary'" dark>
+        <v-list-item prepend-avatar="../src/assets/logo.png">
+          <v-list-item-avatar>
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link to="/account">
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              {{ loggedUser ? loggedUser.name : menuItems[5].text }}
+            </v-list-item-title>
+            <v-list-item-subtitle></v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list nav :color="'primary'" dark>
+        <v-list-item-group v-model="menuItems" color="primary">
+          <v-list-item
+            v-for="(item, i) in menuItems"
+            :key="i"
+            :prepend-icon="item.icon"
+            :to="item.route"
+            v-show="!item.disabled"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app :color="notLoggedIn ? 'primary' : 'loginedBackground'" dark>
@@ -146,16 +171,16 @@ function toggleLanguage() {
 </template>
 
 <style>
-#app {
-  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
-}
+  #app {
+    font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  }
 
-.dark {
-  background-color: #36443c;
-  color: white;
-}
-.default {
-  background-color: #a5b452;
-  color: black;
-}
+  .dark {
+    background-color: #36443c;
+    color: white;
+  }
+  .default {
+    background-color: #a5b452;
+    color: black;
+  }
 </style>
