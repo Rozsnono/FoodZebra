1172b9e1c5759c78ec7ba9fc5bae4cded6443aae
rozsnono <rozsnono@gmail.com>
2022-03-21 09:54:30 +0100

    Gyors fix

--- src/views/FoodAdd.vue
+++ src/views/FoodAdd.vue
@@ -1,125 +1,116 @@
 <script setup lang="ts">
-import { ref } from "@vue/reactivity";
-import { useRecipeStore } from "../store/recipeStore";
-import rating from "../components/Rating.vue";
-import ConfirmDialog from "../components/ConfirmDialog.vue";
+  import { ref } from "@vue/reactivity";
+  import { useRecipeStore } from "../store/recipeStore";
+  import rating from "../components/Rating.vue";
+  import ConfirmDialog from "../components/ConfirmDialog.vue";
 
-const props = defineProps<{ item: object; isModify: boolean; title: boolean }>();
+  const props = defineProps<{ item: object; isModify: boolean; title: boolean }>();
 
-const emit = defineEmits(["close"]);
+  const emit = defineEmits(["close"]);
 
-const name = ref(props.item ? props.item.name : "");
-const type = ref(props.item ? props.item.type : "");
-const description = ref(props.item ? props.item.description : "");
-const energy = ref(props.item ? props.item.energy : "");
-const time = ref(props.item ? props.item.time : "");
-const price = ref(props.item ? props.item.price : "");
-const serving = ref(props.item ? props.item.serving : "");
-const difficulty = ref(props.item ? props.item.difficulty : "");
-const rate = ref(props.item ? props.item.rate : 0);
-const image = ref(props.item ? props.item.pic : "");
-const ingredients = ref(props.item ? props.item.ingredients.join("\n") : "");
-const userID = ref(sessionStorage.getItem("currentUser")?.split(",")[0]);
+  const recipe = ref(props.item);
 
-const setImage = (img) => {
-  image.value = img;
-};
+  const name = ref(recipe ? recipe.name : "");
+  const type = ref(recipe ? recipe.type : "");
+  const description = ref(recipe ? recipe.description : "");
+  const energy = ref(recipe ? recipe.energy : "");
+  const time = ref(recipe ? recipe.time : "");
+  const price = ref(recipe ? recipe.price : "");
+  const serving = ref(recipe ? recipe.serving : "");
+  const difficulty = ref(recipe ? recipe.difficulty : "");
+  const rate = ref(recipe ? recipe.rate : 0);
+  const image = ref(recipe ? recipe.pic : "");
+  const ingredients = ref(recipe ? recipe.ingredients.join("\n") : "");
+  const userID = ref(sessionStorage.getItem("currentUser")?.split(",")[0]);
 
-const setPrice = (getprice) => {
-  price.value = getprice;
-};
-
-const setServing = (getserving) => {
-  serving.value = getserving;
-};
-
-const setDifficulty = (getdifficulty) => {
-  difficulty.value = getdifficulty;
-};
-
-const recipeStore = useRecipeStore();
-
-const showConfirmSave = ref(false);
-const showConfirmEdit = ref(false);
-const showConfirmClose = ref(false);
-const resultConfirm = ref(true);
-const showConfirm = ref(false);
-const showError = ref(false);
-
-async function confirmSaveRecipe() {
-  if (resultConfirm.value) {
-    await recipeStore.createNewRecipe({
-      name: name.value,
-      type: type.value,
-      description: description.value,
-      energy: parseInt(energy.value.toString()),
-      time: parseInt(time.value.toString()),
-      price: price.value,
-      serving: serving.value,
-      difficulty: difficulty.value,
-      pic: image.value,
-      rate: rate.value,
-      author: userID.value ? userID.value : "",
-      ingredients: ingredients.value.split("\n"),
-      _id: "",
-    });
-    let ok = await recipeStore.getCode;
-
-    if (ok != 200) {
-      showError.value = true;
-    } else {
-      showConfirm.value = true;
-    }
-  } else {
-    showConfirmSave.value = false;
-  }
-}
-
-async function confirmEditRecipe() {
-  if (resultConfirm.value) {
-    await recipeStore.editRecipeById({
-      name: name.value,
-      type: type.value,
-      description: description.value,
-      energy: parseInt(energy.value.toString()),
-      time: parseInt(time.value.toString()),
-      price: price.value,
-      serving: serving.value,
-      difficulty: difficulty.value,
-      pic: image.value,
-      ingredients: ingredients.value.split("\n"),
-      _id: props.item._id,
-    });
-    const ok = recipeStore.getCode;
-
-    if (ok != 200) {
-      showError.value = true;
-    } else {
-      showConfirmEdit.value = true;
-    }
-  } else {
-    showConfirmSave.value = false;
-  }
-}
-
-function Tobase64(event) {
-  const selected = event.target?.files[0];
-  const reader = new FileReader();
-  let rawFile = "";
-  reader.onloadend = () => {
-    rawFile = reader.result;
-    setImage(rawFile.split(",")[1]);
+  const setImage = (img) => {
+    image.value = img;
   };
-  reader.readAsDataURL(selected);
-}
 
-function ChooseDifficultyRate(value) {
-  setDifficulty(value);
-}
+  const setDifficulty = (getdifficulty) => {
+    difficulty.value = getdifficulty;
+  };
 
-function confirm() {
-  emit("close");
-}
+  const recipeStore = useRecipeStore();
+
+  const showConfirmSave = ref(false);
+  const showConfirmEdit = ref(false);
+  const resultConfirm = ref(true);
+  const showConfirm = ref(false);
+  const showError = ref(false);
+
+  async function confirmSaveRecipe() {
+    if (resultConfirm.value) {
+      await recipeStore.createNewRecipe({
+        name: name.value,
+        type: type.value,
+        description: description.value,
+        energy: parseInt(energy.value.toString()),
+        time: parseInt(time.value.toString()),
+        price: price.value,
+        serving: serving.value,
+        difficulty: difficulty.value,
+        pic: image.value,
+        rate: rate.value,
+        author: userID.value ? userID.value : "",
+        ingredients: ingredients.value.split("\n"),
+        _id: "",
+      });
+      let ok = await recipeStore.getCode;
+
+      if (ok != 200) {
+        showError.value = true;
+      } else {
+        showConfirm.value = true;
+      }
+    } else {
+      showConfirmSave.value = false;
+    }
+  }
+
+  async function confirmEditRecipe() {
+    if (resultConfirm.value) {
+      await recipeStore.editRecipeById({
+        name: name.value,
+        type: type.value,
+        description: description.value,
+        energy: parseInt(energy.value.toString()),
+        time: parseInt(time.value.toString()),
+        price: price.value,
+        serving: serving.value,
+        difficulty: difficulty.value,
+        pic: image.value,
+        ingredients: ingredients.value.split("\n"),
+        _id: recipe._id,
+      });
+      const ok = recipeStore.getCode;
+
+      if (ok != 200) {
+        showError.value = true;
+      } else {
+        showConfirmEdit.value = true;
+      }
+    } else {
+      showConfirmSave.value = false;
+    }
+  }
+
+  function Tobase64(event) {
+    const selected = event.target?.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImage(reader.result?.toString().split(",")[1]);
+    };
+    reader.readAsDataURL(selected);
+  }
+
+  function ChooseDifficultyRate(value) {
+    setDifficulty(value);
+  }
+
+  function confirm() {
+    emit("close");
+  }
 </script>
 
 <template>
@@ -129,7 +120,7 @@ function confirm() {
         <h1 class="title">{{ props.title ? props.title : "Add new recipe" }}</h1>
         <v-divider></v-divider>
       </v-col>
-      <v-col cols="12" sm="12" lg="6">
+      <v-col cols="12" lg="6" sm="12">
         <v-text-field
           v-model="name"
           class="inputs"
@@ -146,16 +137,16 @@ function confirm() {
           prepend-icon="mdi-food-fork-drink"
         ></v-text-field>
       </v-col>
-      <v-col cols="12" sm="12" lg="12">
+      <v-col cols="12" lg="12" sm="12">
         <v-file-input
           accept="image/*"
-          truncate-length="15"
-          @change="Tobase64($event)"
           label="Picture (max. 50 Kb)"
           prepend-icon="mdi-panorama-variant-outline"
+          truncate-length="15"
+          @change="Tobase64($event)"
         ></v-file-input>
       </v-col>
-      <v-col cols="12" sm="12" lg="6">
+      <v-col cols="12" lg="6" sm="12">
         <v-textarea
           v-model="description"
           class="inputs"
@@ -165,7 +156,7 @@ function confirm() {
           shaped
         ></v-textarea>
       </v-col>
-      <v-col cols="12" sm="12" lg="6">
+      <v-col cols="12" lg="6" sm="12">
         <v-textarea
           v-model="ingredients"
           class="inputs"
@@ -174,7 +165,7 @@ function confirm() {
           shaped
         ></v-textarea>
       </v-col>
-      <v-col cols="12" sm="12" lg="4">
+      <v-col cols="12" lg="4" sm="12">
         <v-text-field
           v-model="energy"
           class="inputs"
@@ -182,7 +173,7 @@ function confirm() {
           prepend-icon="mdi-lightning-bolt-circle"
         ></v-text-field>
       </v-col>
-      <v-col cols="12" sm="12" lg="4">
+      <v-col cols="12" lg="4" sm="12">
         <rating
           title="Difficulty"
           @rating="ChooseDifficultyRate"
@@ -190,7 +181,7 @@ function confirm() {
           :justShow="true"
         ></rating>
       </v-col>
-      <v-col cols="12" sm="12" lg="4">
+      <v-col cols="12" lg="4" sm="12">
         <v-text-field
           v-model="time"
           class="inputs"
@@ -200,90 +191,90 @@ function confirm() {
       </v-col>
 
       <v-spacer></v-spacer>
-      <v-col cols="12" sm="12" lg="3"></v-col>
-      <v-col cols="12" sm="12" lg="6">
+      <v-col cols="12" lg="3" sm="12"></v-col>
+      <v-col cols="12" lg="6" sm="12">
         <v-btn
-          color="green-lighten-3"
+          v-if="props.isModify"
           class="btn"
+          color="green-lighten-3"
           elevation="10"
           @click="confirmEditRecipe()"
-          v-if="props.isModify"
         >
           Edit
         </v-btn>
 
         <v-btn
-          color="green-lighten-3"
+          v-if="!props.isModify"
           class="btn"
+          color="green-lighten-3"
           elevation="10"
           @click="confirmSaveRecipe()"
-          v-if="!props.isModify"
         >
           Save
         </v-btn>
       </v-col>
-      <v-col cols="12" sm="12" lg="3"></v-col>
+      <v-col cols="12" lg="3" sm="12"></v-col>
     </v-row>
     <ConfirmDialog
       v-if="showError"
       v-model="showError"
       v-model:result="resultConfirm"
-      :retain-focus="false"
-      title="Opss.."
-      okBtn="OK"
+      color="danger"
       :justAccept="true"
       message="Something went wrong!"
-      color="danger"
+      okBtn="OK"
+      :retain-focus="false"
+      title="Opss.."
     />
     <ConfirmDialog
       v-if="showConfirm"
       v-model="showConfirm"
       v-model:result="resultConfirm"
-      :retain-focus="false"
-      title="Successful add"
-      okBtn="OK"
       :justAccept="true"
       message="The recipe is added successfully!"
+      okBtn="OK"
+      :retain-focus="false"
+      title="Successful add"
       @close="confirm"
     />
     <ConfirmDialog
       v-if="showConfirmEdit"
       v-model="showConfirmEdit"
       v-model:result="resultConfirm"
-      :retain-focus="false"
-      title="Successful modify"
-      okBtn="OK"
       :justAccept="true"
       message="The recipe is modified successfully!"
+      okBtn="OK"
+      :retain-focus="false"
+      title="Successful modify"
       @close="confirm"
     />
   </v-container>
 </template>
 
 <style scoped>
-.card {
-  /* width: 80rem !important; */
-  background-color: #00000030;
-  border-radius: 2rem;
-  padding: 2rem;
-}
+  .card {
+    /* width: 80rem !important; */
+    background-color: #00000030;
+    border-radius: 2rem;
+    padding: 2rem;
+  }
 
-#file-input {
-  text-align: center !important;
-}
-.title {
-  font-size: 4rem;
-  text-align: center;
-  margin: 2.5rem;
-  display: flex;
-  justify-content: center;
-}
+  #file-input {
+    text-align: center !important;
+  }
+  .title {
+    font-size: 4rem;
+    text-align: center;
+    margin: 2.5rem;
+    display: flex;
+    justify-content: center;
+  }
 
-.btn {
-  width: 100%;
-}
+  .btn {
+    width: 100%;
+  }
 
-.inputs {
-  margin: 0.5rem;
-}
+  .inputs {
+    margin: 0.5rem;
+  }
 </style>
